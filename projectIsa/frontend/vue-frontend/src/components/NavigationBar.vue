cb6a4b10a6cdb4d236c2095acc635439a545718c
Ivana Bugarski <bugarski.ra57.2018@uns.ac.rs>
2022-08-13 18:09:46 +0200

    feat: #3.5.2 login for owners done

--- projectIsa/frontend/vue-frontend/src/components/NavigationBar.vue
+++ projectIsa/frontend/vue-frontend/src/components/NavigationBar.vue
@@ -7,13 +7,16 @@
                         <router-link v-if="isLoggedIn ==='Client'" to="/client-home-page" class="nav-item nav-link">Home</router-link>
                         <router-link v-if="isLoggedIn ==='Administrator'" to="/admin-home-page" class="nav-item nav-link">Home</router-link>
                         <router-link v-if="isLoggedIn ==='FishingInstructor'" to="/instructor-home-page" class="nav-item nav-link">Home</router-link>
+                        <router-link v-if="isLoggedIn ==='BoatOwner'" to="/boatOwner-home-page" class="nav-item nav-link">Home</router-link>
+                        <router-link v-if="isLoggedIn ==='CottageOwner'" to="/cottageOwner-home-page" class="nav-item nav-link">Home</router-link>
                     </div>
                     <div class="navbar-nav ms-auto">
                         <router-link v-if="isLoggedIn ==='Client'" to="/client-reservations-history" class="nav-item nav-link">Reservations</router-link>
-                        <router-link v-if="isLoggedIn ==='Client'" to="/client-profile" class="nav-item nav-link">Profile</router-link>                        
+                        <router-link v-if="isLoggedIn ==='Client'" to="/client-profile" class="nav-item nav-link">Profile</router-link>
+                        <router-link v-if="isLoggedIn ==='BoatOwner'" to="/boatOwner-profile" class="nav-item nav-link">Boat owner profile</router-link>                        
                         <router-link v-if="!isLoggedIn" to="/registration-choice" class="nav-item nav-link">Registration</router-link>
                         <router-link v-if="!isLoggedIn" to="/login" class="nav-item nav-link">Login</router-link>
-                        <router-link v-if="isLoggedIn ==='Client' || isLoggedIn ==='Administrator' || isLoggedIn ==='FishingInstructor'"  v-on:click="logout" to="/" class="nav-item nav-link">Logout</router-link>
+                        <router-link v-if="isLoggedIn ==='Client' || isLoggedIn ==='Administrator' || isLoggedIn ==='FishingInstructor' || isLoggedIn ==='BoatOwner' || isLoggedIn ==='CottageOwner'"  v-on:click="logout" to="/" class="nav-item nav-link">Logout</router-link>
                     </div>
             </nav>
         </div>
